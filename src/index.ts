9d7b805bd5bcf3019830f81856fb6d487274ded7
MEM3 <business.leo@gmx.net>
2022-11-21 14:09:27 +0100

    Second Repository test

--- src/index.ts
+++ src/index.ts
@@ -6,5 +6,3 @@ title.textContent = "PassSafe"
 const body = document.querySelector("body")
 const appComponent = document.createElement("app-component")
 body.appendChild(appComponent)
-
-console.log("TEST")
