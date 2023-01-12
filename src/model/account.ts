2b2213875d1fa8b8b53a28ddd6e9afad4ac47681
Leo <83591526+M3-13@users.noreply.github.com>
2023-01-12 09:23:16 +0000

    possibility to generate password automatically

--- src/model/account.ts
+++ src/model/account.ts
@@ -2,6 +2,7 @@ export interface Account {
     readonly name?: string,
     readonly username?: string,
     readonly email?: string,
-    readonly password: string
+    readonly password: string,
     readonly url?: string,
+    readonly creationDate?: Date
 }
